ac6f0fb923ea397fb212495b45ff774b760078f3
joao marcelo <joaomarcelodaltromarques@hotmail.com>
2024-07-05 12:13:15 -0300

    Organização de projeto

---
+++ System.Entities/Account/Account.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Entities
+{
+  public class Account : DefaultDB
+  {
+
+    [Required]
+    [StringLength(100, MinimumLength = 6, ErrorMessage = "The Login must be at least 6 characters long.")]
+    public String Login { get; set; } = "";
+
+    [Required]
+    [StringLength(100, MinimumLength = 6, ErrorMessage = "The Password must be at least 6 characters long.")]
+    public string Password { get; set; } = "";
+
+    [Required]
+    [EmailAddress(ErrorMessage = "The email address is not valid.")]
+    public string Email { get; set; } = "";
+  }
+}
---
+++ System.Entities/Base/DefaultDB.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Entities
+{
+  public class DefaultDB
+  {
+    [Key]
+    public int Id { get; set; }
+
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public Guid UniqueId { get; set; }
+    public bool IsDeleted { get; set; }
+    public DateTime DateDeleted{ get; set; }
+    public bool IsUpdated { get; set; }
+    public DateTime DateUpdated { get; set; }
+  }
+}
